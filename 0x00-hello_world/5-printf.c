cff17eca448c73b24cf1b24ad5d79c18c48b419b
mohamedwalidghlwash <m7m01149909092@gmail.com>
2023-03-16 11:06:01 -0700

    dfgh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- *Description: A C program that prints with printf function
+ *Description: A C program that prints with printf function.
  *
  *Return: Always 0 (success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of ar,\n");
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
